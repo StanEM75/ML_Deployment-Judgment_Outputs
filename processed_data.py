a2f45a340d276e9fc7218b6b52e01aec41bd4b92
Pierre MULLER <60134228+PideurM@users.noreply.github.com>
2024-09-19 17:16:20 +0200

    REVIEWED - Processing-Stan - Merge Pierre
    Co-authored-by: Stanislas <55952159+StanEM75@users.noreply.github.com>

--- processed_data.py
+++ processed_data.py
@@ -1,67 +1,11 @@
 import os
 import sys
 import time
-
 import pandas as pd
-from utils.utils import upper_case_strip
 
-
-def is_judgement_later(row):
-    return 0 if (pd.isnull(row['judgment_date']) or row['judgment_date'] <= row['hearing_date']) else \
-           1 if row['judgment_date'].month == row['hearing_date'].month else \
-           2 if row['judgment_date'].year == row['hearing_date'].year else 3
-
-def processed_data_for_data_science(df : pd.DataFrame, columns_to_drop : list) -> pd.DataFrame:
-
-    '''
-    This function takes in a dataframe and processes it for our data science model. 
-    Our data science project is to predict whether a blight violation will be paid or not, if the the violator is responsible for its own violation.
-
-    The function does the following:
-
-
-    
-    '''
-     # Drop nan values in specific columns
-    df = df.dropna(subset=columns_to_drop_only_NaN)
-
-    df['judgment_date'] = pd.to_datetime(df['judgment_date'],format='mixed').dt.date
-    df['hearing_date'] = pd.to_datetime(df['hearing_date'],format='mixed').dt.date
-    
-    # Violation date to datetime
-    df['violation_date'] = pd.to_datetime(df['violation_date'], errors='coerce')
-
-    # Delete rows where violation_date is null or superior to 2024
-    df = df[(df['violation_date'].notnull()) & (df['violation_date'] < '2023-01-01') & (df['violation_date'] > '2004-12-31')]
-
-    # Create only two categories for payment_status : NO PAYMENT & PAID
-    df['payment_status'] = df['payment_status'].apply(lambda x: 'NO PAYMENT' if x not in ['PAID IN FULL'] else x)
-
-    # Create a new column to see if the judgment date is later than the hearing date
-    df['is_judgment_later'] = df.apply(is_judgement_later, axis=1)
-
-    # Drop NEIGHBORHOOD CITY HALLS  
-    df = df[df['agency_name'] != 'NEIGHBORHOOD CITY HALLS']
-
-    # Drop all rows where counts of each disposition is less than 1000
-    # We only take violators who were considered responsible because it seems to be normal to not pay if you are not guilty
-    df = df.groupby('disposition').filter(lambda x: len(x) > 10000)
-    
-    df['disposition'] = df.loc[~df['disposition'].str.contains('NOT RESPONSIBLE', case=False)]['disposition']
-
-    # Drop columns that are not needed to create our model. It is due to uncessary data that could lead to overfitting, data privacy or incomplete data.
-    df = df.drop(columns_to_drop, axis=1)
-
-    # For each column with object type, each value is transformed in uppercase
-    df[[col for col in df.columns if df[col].dtype == 'O']] = df[[col for col in df.columns if df[col].dtype == 'O']].map(upper_case_strip)
-
-    return df
-
-
-
+from my_model_package.forecasting.utils import pre_processing
 
 if __name__ == '__main__':
-    import os
     print(os.getcwd())
 
     df = pd.read_csv('data/blight_violations.csv')
